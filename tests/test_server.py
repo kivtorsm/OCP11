df6d25ea577c101544e99502ebc7615203a5d2ed
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-06-23 12:23:43 +0200

    added message when trying to book past events

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,4 +1,5 @@
 import pytest
+import datetime
 
 from bs4 import BeautifulSoup
 
@@ -38,8 +39,7 @@ def club(request):
         "name": "Club Mock",
         "email": "Email Mock",
         "points": "5"
-    },
-    {
+    }, {
         "name": "Club Mock",
         "email": "Email Mock",
         "points": "13"
@@ -49,39 +49,34 @@ def club(request):
 
 
 @pytest.fixture
-def club2():
-    club = {
-        "name": "Club Mock",
-        "email": "Email Mock",
-        "points": "13"
-    }
-    return club
-
-
-@pytest.fixture
-@pytest.mark.parametrize("club", [0, 1], indirect=True)
+@pytest.mark.parametrize("club", [0, 1], indirect=['club'])
 def clubs(club):
     clubs = [club]
     return clubs
 
 
 @pytest.fixture
-def competition():
-    competition = {
+def competition(request):
+    competitions = [{
         "name": "Competition Mock",
         "date": "2020-10-22 13:30:00",
         "numberOfPlaces": "25"
-    }
-    return competition
+    }, {
+        "name": "Competition Mock",
+        "date": "2024-10-22 13:30:00",
+        "numberOfPlaces": "24"
+    }]
+    return competitions[request.param]
 
 
 @pytest.fixture
+@pytest.mark.parametrize("competition", [0, 1], indirect=['competition'])
 def competitions(competition):
     competitions = [competition]
     return competitions
 
 
-@pytest.mark.parametrize("club", [0, 1], indirect=True)
+@pytest.mark.parametrize("club,competition", [(0, 1), (1, 1)], indirect=['club', 'competition'])
 def test_should_check_max_input(mocker, client, club, competition, clubs, competitions):
     mocker.patch.object(server, 'clubs', clubs)
     mocker.patch.object(server, 'competitions', competitions)
@@ -90,6 +85,7 @@ def test_should_check_max_input(mocker, client, club, competition, clubs, compet
 
     response = client.get(f'/book/{competition["name"]}/{club["name"]}')
     response_soup = BeautifulSoup(response.data.decode(), 'html.parser')
+    print(response_soup)
     input_field = response_soup.find("input", {"name": "places"})
     print(input_field)
     input_max_places = input_field.attrs["max"]
@@ -99,88 +95,7 @@ def test_should_check_max_input(mocker, client, club, competition, clubs, compet
     assert int(input_max_places) == expected_result
 
 
-@pytest.mark.parametrize("club", [0], indirect=True)
-def test_should_validate_point_and_places_substraction(mocker, client, club, competition, clubs, competitions):
-
-    mocker.patch.object(server, 'clubs', clubs)
-    mocker.patch.object(server, 'competitions', competitions)
-    response = client.post('/purchasePlaces', data={
-        "places": "5",
-        "competition": competition["name"],
-        "club": club["name"],
-    })
-    print(response.data.decode())
-    expected_result = 0
-    assert int(club["points"]) == expected_result
-
-@pytest.fixture
-def club(request):
-    clubs = [{
-        "name": "Club Mock",
-        "email": "Email Mock",
-        "points": "5"
-    },
-    {
-        "name": "Club Mock",
-        "email": "Email Mock",
-        "points": "13"
-    }
-    ]
-    return clubs[request.param]
-
-
-@pytest.fixture
-def club2():
-    club = {
-        "name": "Club Mock",
-        "email": "Email Mock",
-        "points": "13"
-    }
-    return club
-
-
-@pytest.fixture
-@pytest.mark.parametrize("club", [0, 1], indirect=True)
-def clubs(club):
-    clubs = [club]
-    return clubs
-
-
-@pytest.fixture
-def competition():
-    competition = {
-        "name": "Competition Mock",
-        "date": "2020-10-22 13:30:00",
-        "numberOfPlaces": "25"
-    }
-    return competition
-
-
-@pytest.fixture
-def competitions(competition):
-    competitions = [competition]
-    return competitions
-
-
-@pytest.mark.parametrize("club", [0, 1], indirect=True)
-def test_should_check_max_input(mocker, client, club, competition, clubs, competitions):
-    mocker.patch.object(server, 'clubs', clubs)
-    mocker.patch.object(server, 'competitions', competitions)
-    print(club)
-    print(clubs)
-
-    response = client.get(f'/book/{competition["name"]}/{club["name"]}')
-    response_soup = BeautifulSoup(response.data.decode(), 'html.parser')
-    input_field = response_soup.find("input", {"name": "places"})
-    print(input_field)
-    input_max_places = input_field.attrs["max"]
-    print(input_max_places)
-    expected_result = min(12, int(club['points']))
-
-    assert int(input_max_places) == expected_result
-
-
-@pytest.mark.parametrize("club", [0], indirect=True)
+@pytest.mark.parametrize("club, competition", [(0, 0)], indirect=['club', 'competition'])
 def test_should_validate_point_and_places_substraction(mocker, client, club, competition, clubs, competitions):
     mocker.patch.object(server, 'clubs', clubs)
     mocker.patch.object(server, 'competitions', competitions)
@@ -189,8 +104,20 @@ def test_should_validate_point_and_places_substraction(mocker, client, club, com
         "competition": competition["name"],
         "club": club["name"],
     })
-    print(response.data.decode())
     expected_result_points = 0
     expected_result_places = 20
     assert int(club["points"]) == expected_result_points
     assert int(competition["numberOfPlaces"]) == expected_result_places
+
+
+class TestShowSummary:
+    @pytest.mark.parametrize("club, competition", [(0, 0)], indirect=['club', 'competition'])
+    def test_booking_past_events_should_show_error(self, client, mocker, club, competition, clubs, competitions):
+        """
+        Tests an error is shown if trying to book an event in the past.
+        """
+        mocker.patch.object(server, 'clubs', clubs)
+        mocker.patch.object(server, 'competitions', competitions)
+        response = client.get(f'/book/{competition["name"]}/{club["name"]}')
+        expected_value = 'You can&#39;t book events in the past'
+        assert expected_value in response.data.decode('unicode_escape')
